e30fa9e4f051acb61cfa7d4aceb6382f9bb8fe25
UruvielNaomi <naomi.stacia@hotmail.com>
2023-11-22 13:49:36 +0100

    Removed testfile to check if github has been properly set up.
    Set up the basic outline of the project and its files.
    
    created a 'notes and concept file' with explanation of the concepts we need to use.
    
    added the 42Header to all files.

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nstacia <nstacia@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 13:47:42 by nstacia           #+#    #+#             */
+/*   Updated: 2023/11/22 13:48:00 by nstacia          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char *get_next_line(int fd)
+{
+
+}
+
+/*
+
+Parameters 
+	fd: The file descriptor to read from
+
+Return value 
+	Read line: correct behavior
+	NULL: there is nothing else to read, or an error occurred
+
+External functs. 
+	read, malloc, free
+
+Description 
+	Write a function that returns a line read from a file descriptor
+
+*/
