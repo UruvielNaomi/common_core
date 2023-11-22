e6198156a27dfb837981007e181a8854f07c1fca
UruvielNaomi <naomi.stacia@hotmail.com>
2023-11-22 15:27:36 +0100

    updated notes for get_next_line.

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,19 @@
 /*   By: nstacia <nstacia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 13:47:49 by nstacia           #+#    #+#             */
-/*   Updated: 2023/11/22 13:47:57 by nstacia          ###   ########.fr       */
+/*   Updated: 2023/11/22 14:43:20 by nstacia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <stdarg.h>
+# include <unistd.h>
+# include <limits.h>
+
+char	*get_next_line(int fd);
+
+#endif
