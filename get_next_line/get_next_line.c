c2997e3bcf57ac6b5e59dcc088dfa0cc76eab1a7
UruvielNaomi <naomi.stacia@hotmail.com>
2023-12-06 14:32:59 +0100

    added a main.c to the comments_folder to test my code.

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: nstacia <nstacia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 08:53:43 by nstacia           #+#    #+#             */
-/*   Updated: 2023/12/06 12:18:56 by nstacia          ###   ########.fr       */
+/*   Updated: 2023/12/06 14:32:04 by nstacia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*process_buffer(char **remaining)
+static char	*process_and_move(char **remaining)
 {
 	char	*line;
 	char	*newline_char;
@@ -71,44 +71,6 @@ char	*get_next_line(int fd)
 	char		*line;
 
 	remaining = read_and_buffer(fd, remaining);
-	line = process_buffer(&remaining);
+	line = process_and_move(&remaining);
 	return (line);
 }
-/*
-int main(void)
-{
-    int fd = open("test_file.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fd == -1)
-    {
-        perror("Error creating file");
-        return 1;
-    }
-    const char *text = "This is line 1.\nThis is line 2.\nThis is line 3.";
-    if (write(fd, text, ft_strlen(text)) == -1)
-    {
-        perror("Error writing to file");
-        close(fd);
-        return 1;
-    }
-    close(fd);
-    fd = open("test_file.txt", O_RDONLY);
-    if (fd == -1)
-    {
-        perror("Error opening file");
-        return 1;
-    }
-    char *line;
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("Line read: %s\n", line);
-        free(line);
-    }
-    close(fd);
-    return (0);
-}*/
-
-/*
-cc -Wall -Wextra -Werror -D BUFFER_SIZE=42 
-get_next_line.c get_next_line_utils.c -o gnl
-
-*/
