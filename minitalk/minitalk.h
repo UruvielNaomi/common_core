cd4ce47be37c6c78ae0415aa32c2f6dbe397d412
UruvielNaomi <naomi.stacia@hotmail.com>
2024-03-21 16:43:59 +0100

    updated structure name for clarity

--- minitalk/minitalk.h
+++ minitalk/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: Naomi <Naomi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 14:50:40 by nstacia           #+#    #+#             */
-/*   Updated: 2024/03/21 14:57:47 by Naomi            ###   ########.fr       */
+/*   Updated: 2024/03/21 16:43:46 by Naomi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 struct {
 	int		bits;
 	char	character;
-}			s_received;
+}			s_server;
 
 int	ft_atoi(const char *str);
 
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: Naomi <Naomi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 14:47:17 by nstacia           #+#    #+#             */
-/*   Updated: 2024/03/21 14:58:07 by Naomi            ###   ########.fr       */
+/*   Updated: 2024/03/21 16:41:53 by Naomi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,22 @@
 void	bit_to_string(int signum)
 {
 	if (signum == SIGUSR2)
-		s_received.character += (1 << s_received.bits);
-	s_received.bits++;
-	if (s_received.bits == 8)
+		s_server.character += (1 << s_server.bits);
+	s_server.bits++;
+	if (s_server.bits == 8)
 	{
-		ft_printf("%c", s_received.character);
-		if (s_received.character == '\0')
+		ft_printf("%c", s_server.character);
+		if (s_server.character == '\0')
 			ft_printf("\n");
-		s_received.bits = 0;
-		s_received.character = 0;
+		s_server.bits = 0;
+		s_server.character = 0;
 	}
 }
 
 int	main(void)
 {
-	s_received.bits = 0;
-	s_received.character = 0;
+	s_server.bits = 0;
+	s_server.character = 0;
 	ft_printf("Welcome in the server.\n");
 	ft_printf("Server PID is: %d\n", getpid());
 	while (1)
