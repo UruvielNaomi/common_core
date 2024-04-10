4e0ac05435ab2f4815e4e2880e50155956b82e8c
UruvielNaomi <naomi.stacia@hotmail.com>
2024-04-10 09:05:02 +0200

    added checker_mac added ft_slit

--- push_swap_linked_list/libft/libft.h
+++ push_swap_linked_list/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: Naomi <Naomi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 13:18:37 by Naomi             #+#    #+#             */
-/*   Updated: 2024/04/05 19:29:59 by Naomi            ###   ########.fr       */
+/*   Updated: 2024/04/10 08:15:31 by Naomi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ typedef struct s_list
 	struct s_list	*next;
 }				t_list;
 
+char	**ft_split(char const *s, char c);
 int		ft_atoi(const char *str, int *temp_stack);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 size_t	ft_strlen(const char *str);
